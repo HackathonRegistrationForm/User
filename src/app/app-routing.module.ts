c40e050ddb4c8e9169c1eb1e8d04d4406198d926
Umavengalaraju <umavengalaraju20@gmail.com>
2023-01-08 15:44:55 +0530

    project

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [];
+import { HomeComponent } from './home/home.component';
+import { ViewrecordComponent } from './viewrecord/viewrecord.component';
+import { ProfileComponent } from './profile/profile.component';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
+import { EmployeeformComponent } from './employeeform/employeeform.component';
+import { ContactusComponent } from './contactus/contactus.component';
+import { AboutusComponent } from './aboutus/aboutus.component';
+import { AttendenceComponent } from './attendence/attendence.component';
+const routes: Routes =[
+  {path:'home',component:HomeComponent},
+  {path:'attendence',component:AttendenceComponent},
+  {path:'about',component:AboutusComponent},
+  {path:'contact',component:ContactusComponent},
+  {path:'profile',component:ProfileComponent},
+  {path:'login',component:LoginComponent},
+  {path:'signup',component:SignupComponent},
+  {path:'employeeform',component:EmployeeformComponent},
+  {path:'viewrecord',component:ViewrecordComponent},
+  {path:'home',component:HomeComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
